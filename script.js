b6e018ce0e10a77ed506dd200f7eb110070f23f0
Johntizzy <adepojujohn566@gmail.com>
2024-10-21 17:25:53 +0100

    inital commit

---
+++ script.js
@@ -0,0 +1,45 @@
+// // index.js
+
+// import data from './data.json' //assert { type: 'json' };
+
+// console.log(data); // Outputs the entire JSON object
+// console.log(data.title); // Outputs: Hello World
+
+// index.js
+
+const dessertContainer = document.getElementById("dessert-cont");
+
+fetch("./data.json") // Adjust the path if necessary
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok " + response.statusText);
+    }
+    return response.json(); // Parse the JSON data
+  })
+  .then((Data) => {
+    let desserts = Data.map(
+      (data) =>
+        `
+                <div class="card dessertItems">
+                    <img src=${
+                      (window.innerWidth <= 600)
+                        ? data.image.mobile
+                        : data.image.desktop
+                    } />
+                    <div class="addTocart">
+                      <button>Add to cart</button>
+                    </div>
+                    <p>${data.category}</p>
+        
+                    <h2>${data.name}</h2>
+                    <h2 class="price">$${data.price}</h2>
+                  </div>
+                `
+    );
+    dessertContainer.innerHTML = desserts;
+
+    // dessertContainer.innerHTML = desserts
+  })
+  .catch((error) => {
+    console.error("There was a problem with the fetch operation:", error);
+  });
